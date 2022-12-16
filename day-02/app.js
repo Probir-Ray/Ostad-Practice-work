0816519ab514be1d47d21531ebf8109dd831f606
Probir-Ray <somironroy.need@gmail.com>
2022-12-16 17:27:20 +0600

    update 2

---
+++ day-02/app.js
@@ -0,0 +1,67 @@
+
+'use strict';
+/*
+myName();
+function myName(){
+    let name = 'Krishna Chandra'
+    console.log(name);
+}
+
+
+
+// Spread Operator for array
+const arr_one = ["BD", "IND", "PAK"];
+const arr_two = ["UK", "USA", "CA"];
+const arr_three = ["RU", "CH",  ...arr_one, "GR", ...arr_two, "FR"];
+console.log(arr_one);
+console.log(arr_two);
+console.log(arr_three);
+
+
+
+
+// Rest Operator
+
+function sumOfNumbers(a, b, ...all) {
+    let sum = 0;
+    for(let el of all) {
+        sum += el;
+    }
+    console.log(sum)
+}
+sumOfNumbers(2, 3, -11, 9, 2, 5, 27);
+
+
+
+// Dynamic function or Function expression
+
+const sum = function(a, b) {
+    return a + b;
+}
+console.log(sum);
+console.log(sum(4, 7));
+
+
+
+// Scope :: Local Scope :: Global Scope
+let fruit = 'Mango';
+const sampleFun = () => {
+    console.log(fruit);
+}
+sampleFun()
+console.log(`Fruit is`, fruit);
+
+
+
+
+
+// Variable Hoisting
+country = 'Dhaka';
+console.log(country);
+var country;
+
+
+*/
+
+
+
---
+++ day-03-practice/app.js
@@ -0,0 +1,9 @@
+const bd = {
+    capital: "Dhaka",
+    area: "1.47 lac",
+    population: "185 million"
+}
+
+for(let el in bd) {
+    console.log(bd[el]);
+}
